b4965ebf568facadfe858e270286894953fe9c10
ViraatArora <mail.viraatarora@gmail.com>
2025-05-16 03:06:03 +0530

    Path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/IBExcellence/',
+  base: mode === 'production' ? '/IBExcellence/' : '/',
   server: {
     host: "::",
     port: 8080,
