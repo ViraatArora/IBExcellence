64898cad2bccfcc115e6c91ea3ab445565de5c2a
ViraatArora <mail.viraatarora@gmail.com>
2025-05-16 23:30:13 +0530

    Local running

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,13 @@
-<<<<<<< Updated upstream
-
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-=======
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
 
-function App() {
-  return (
-    <div>
-      <Routes>
-        {/* Basic home route */}
-        <Route path="/" element={<div>Welcome to Math Ace Tutoring!</div>} />
-        {/* TODO: add your real routes/components here */}
-      </Routes>
-    </div>
-  );
-}
->>>>>>> Stashed changes
-
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
