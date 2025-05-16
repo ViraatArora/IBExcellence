aa85b7cc8c34ea7d269bffeebcd7640c72f7e1ab
ViraatArora <mail.viraatarora@gmail.com>
2025-05-16 23:18:30 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,23 @@
+<<<<<<< Updated upstream
 
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+=======
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        {/* Basic home route */}
+        <Route path="/" element={<div>Welcome to Math Ace Tutoring!</div>} />
+        {/* TODO: add your real routes/components here */}
+      </Routes>
+    </div>
+  );
+}
+>>>>>>> Stashed changes
 
 createRoot(document.getElementById("root")!).render(<App />);
